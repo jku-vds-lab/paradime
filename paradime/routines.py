e8862430c7e60641d325777a49cc9eeeb21de8b6
einbandi <hinterreiter.a@gmail.com>
2022-08-02 11:26:17 +0200

    improve in_dim inference (closes #29)

--- paradime/routines.py
+++ paradime/routines.py
@@ -1,5 +1,6 @@
 from typing import Optional, Union
 import torch
+import numpy as np
 import sklearn.decomposition
 
 import paradime as prdm
@@ -11,9 +12,51 @@ from paradime.exceptions import NoDatasetRegisteredError
 from paradime.types import Data
 
 class ParametricTSNE(prdm.ParametricDR):
+    """A parametric version of t-SNE.
+
+    This class provides a high-level interface for a
+    :class:`paradime.paradime.ParametricDR` routine with the following
+    specifications:
+    - The global relations are :class:`paradime.relations.NeighborBasedPDist`,
+      transformed with a :class:`paradime.transforms.PerplexityBasedRescale`
+      followed by :class:`paradime.tranforms.Symmetrize`.
+    - The batch relations are :class:`paradime.relations.DifferentiablePDist`,
+      transformed with a :class:`paradime.relations.StudentTTransform`
+      followed by :class:`paradime.transform.Normalize`.
+    
+    Args:
+        in_dim: The numer of dimensions of the input data, used to construct a
+            default model in case none is specified. If a dataset is specified
+            at instantiation, the correct value for this parameter will be
+            inferred from the data dimensions.
+        out_dim: The number of output dimensions (i.e., the dimensionality of
+            the embedding).
+        hidden_dims: Dimensions of hidden layers for the default fully
+            connected model that is created if no model is specified.
+        perplexity: The desired perplexity, which can be understood as
+            a smooth measure of nearest neighbors used to determine
+            high-dimensional relations between data points.
+        model: The model used to embed the high dimensional data.
+        alpha: Degrees of freedom of the Student's t-disitribution used to
+            calculate low-dimensional relations between data points.
+        initialization: How to pretrain the model to mimic initialization of
+            low-dimensional positions. By default (`'pca'`) the model is
+            pretrained to output an approximation of PCA before beginning the
+            main training phase.
+        epochs: The number of epochs in the main training phase.
+        init_epochs: The number of epochs in the pretraining (initialization).
+            phase.
+        batch_size: The number of items in a batch.
+        data_key: The key under which the data can be found in the dataset.
+        dataset: The dataset on which to perform the training. Datasets can be
+            registerd after instantiation using the :meth:`register_dataset`
+            class method.
+        use_cuda: Whether or not to use the GPU for training.
+        verbosity: Verbosity flag.
+    """
 
     def __init__(self,
-        in_dim: int,
+        in_dim: Optional[int] = None,
         out_dim: int = 2,
         hidden_dims: list[int] = [100, 50],
         perplexity: float = 30.,
@@ -31,12 +74,30 @@ class ParametricTSNE(prdm.ParametricDR):
         self.in_dim = in_dim
         self.out_dim = out_dim
         self.hidden_dims = hidden_dims
+        if in_dim is None:
+            if model is None and dataset is None:
+                raise ValueError(
+                    "A value for 'in_dim' must be given if no model or "
+                    "dataset is specified."
+                )
+            elif model is None and dataset is not None:
+                if isinstance(dataset, (np.ndarray, torch.Tensor)):
+                    self.in_dim = dataset[0].shape[-1]
+                elif isinstance(dataset, dict):
+                    self.in_dim = dataset[data_key][0].shape[-1]
+                elif isinstance(dataset, prdm.Dataset):
+                    self.in_dim = dataset[0][data_key].shape[-1]
         if model is None:
-            self.model = pdmod.FullyConnectedEmbeddingModel(
-                self.in_dim,
-                self.out_dim,
-                self.hidden_dims
-            )
+            if isinstance(self.in_dim, int):
+                self.model = pdmod.FullyConnectedEmbeddingModel(
+                    self.in_dim,
+                    self.out_dim,
+                    self.hidden_dims
+                )
+            else:
+                raise ValueError(
+                    "Could not infer 'in_dim' from dataset."
+                )
         else:
             self.model = model
         self.perplexity = perplexity
@@ -50,16 +111,18 @@ class ParametricTSNE(prdm.ParametricDR):
         self.data_key = data_key
 
         global_rel = pdrel.NeighborBasedPDist(
-            transform=pdtf.PerplexityBasedRescale(
-                perplexity=perplexity
-            )
+            transform=[
+                pdtf.PerplexityBasedRescale(
+                    perplexity=perplexity
+                ),
+                pdtf.Symmetrize(),
+            ]
         )
         batch_rel = pdrel.DifferentiablePDist(
             transform=[
                 pdtf.StudentTTransform(alpha=self.alpha),
                 pdtf.Normalize(),
-                pdtf.Symmetrize(),
-                pdtf.ToSquareTensor()
+                pdtf.ToSquareTensor(),
             ]
         )
         super().__init__(
